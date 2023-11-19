f2b1f487588feac488f0d888da773967203f525e
Meoworo <mikkose@icloud.com>
2023-11-19 22:22:38 +0300

    bad wall texturing

--- Assets/Scripts/Map/GridManager.cs
+++ Assets/Scripts/Map/GridManager.cs
@@ -11,13 +11,6 @@ public class GridManager : MonoBehaviour
     [SerializeField]
     private TileBase floorTile;
     
-    //Тайловая карта стен
-    [SerializeField]
-    private Tilemap wallTilemap;
-    //Тайл которым будем закрашивать стены
-    [SerializeField]
-    private TileBase wallTile;
-    
     //Генератор данжей который юзаем
     [SerializeField]
     public GeneratorBase generator;
@@ -25,11 +18,82 @@ public class GridManager : MonoBehaviour
     [SerializeField]
     public WallsGenerator wallsGenerator;
 
+    public Tilemap wallTilemap;
+
+    [System.Serializable]
+    public struct WallTile
+    {
+        public string name;
+        public TileBase tile;
+    }
+
+    // Список стен с названиями
+    public List<WallTile> wallTilesList;
+
+    private Dictionary<string, TileBase> wallTilesDictionary;
+
+    private void InitializeWallTilesDictionary()
+    {
+        wallTilesDictionary = new Dictionary<string, TileBase>();
+        foreach (WallTile wallTile in wallTilesList)
+        {
+            if (!wallTilesDictionary.ContainsKey(wallTile.name))
+            {
+                wallTilesDictionary.Add(wallTile.name, wallTile.tile);
+            }
+            else
+            {
+                // Ошибка
+            }
+        }
+    }
+
+    public void PaintWalls(IEnumerable<Vector2Int> wallPositionsEnumerable)
+    {
+        HashSet<Vector2Int> wallPositions = new HashSet<Vector2Int>(wallPositionsEnumerable);
+        foreach (Vector2Int pos in wallPositions)
+        {
+            Vector3Int tilePosition = new Vector3Int(pos.x, pos.y, 0);
+            TileBase tileToUse = DetermineTileType(pos, wallPositions);
+            if (tileToUse != null)
+            {
+                wallTilemap.SetTile(tilePosition, tileToUse);
+            }
+        }
+    }
+
+    private TileBase DetermineTileType(Vector2Int position, HashSet<Vector2Int> wallPositions)
+    {
+        bool top = wallPositions.Contains(position + Vector2Int.up);
+        bool bottom = wallPositions.Contains(position + Vector2Int.down);
+        bool left = wallPositions.Contains(position + Vector2Int.left);
+        bool right = wallPositions.Contains(position + Vector2Int.right);
+
+        if (top && bottom && left && right && wallTilesDictionary.TryGetValue("defaultWallTile", out TileBase tile)) return tile;
+
+        // Углы
+        if (!top && !left && wallTilesDictionary.TryGetValue("topLeftCorner", out tile)) return tile;
+        if (!top && !right && wallTilesDictionary.TryGetValue("topRightCorner", out tile)) return tile;
+        if (!bottom && !left && wallTilesDictionary.TryGetValue("bottomLeftCorner", out tile)) return tile;
+        if (!bottom && !right && wallTilesDictionary.TryGetValue("bottomRightCorner", out tile)) return tile;
+
+        // Стороны
+        if (top && bottom && !left && !right && wallTilesDictionary.TryGetValue("rightWall", out tile)) return tile;
+        if (top && bottom && !left && !right && wallTilesDictionary.TryGetValue("leftWall", out tile)) return tile;
+        if (left && right && !top && !bottom && wallTilesDictionary.TryGetValue("bottomWall", out tile)) return tile;
+        if (left && right && !top && !bottom && wallTilesDictionary.TryGetValue("topWall", out tile)) return tile;
+
+        
+        if (wallTilesDictionary.TryGetValue("defaultWallTile", out tile)) return tile;
+
+        return null;
+    }
+
     //Передаем позиции
     public void PaintTiles(IEnumerable<Vector2Int> floorPositions, IEnumerable<Vector2Int> wallPositions)
     {
         PaintTiles(floorPositions, floorTilemap, floorTile);
-        PaintTiles(wallPositions, wallTilemap, wallTile);
+        PaintWalls(wallPositions);
     }
 
     //Закрашиваем каждый тайл
@@ -75,6 +139,8 @@ public class GridManager : MonoBehaviour
     //Генерим карту первый раз
     public void Start()
     {
+        InitializeWallTilesDictionary();
+
         Clear();
         var floorPositions = generator.CreateDungeon();
         //Генерирует позиции стен на основе позиций пола
